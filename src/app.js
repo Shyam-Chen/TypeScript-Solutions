014ee759a0f845aea4d75b6b8914317a9b62d96a
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-07 13:23:17 +0800

    7th Commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+// import 'material-components-web/dist/material-components-web';
+
+[].forEach.call(
+  document.querySelectorAll('.mdc-button[data-ripple]'),
+  button => window.mdc.ripple.MDCRipple.attachTo(button),
+);
+
 document.querySelector('#foo').innerHTML = 'Foo';
