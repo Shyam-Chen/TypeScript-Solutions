140e5ad423c633905580dbd85dfe1a6cf65ff6ed
Shyam Chen <shyamchen1994@gmail.com>
2019-07-03 11:50:23 +0800

    14th Commit

--- src/1002. Find Common Characters/common-chars.spec.ts
+++ src/1002. Find Common Characters/common-chars.spec.ts
@@ -2,8 +2,7 @@ import { commonChars } from './common-chars';
 
 describe('commonChars', () => {
   it('commonChars', () => {
-    // TODO: including duplicates
-    // expect(commonChars(['bella', 'label', 'roller'])).toEqual(['e', 'l', 'l']);
+    expect(commonChars(['bella', 'label', 'roller'])).toEqual(['e', 'l', 'l']);
     expect(commonChars(['cool', 'lock', 'cook'])).toEqual(['c', 'o']);
   });
 });
--- src/1002. Find Common Characters/common-chars.ts
+++ src/1002. Find Common Characters/common-chars.ts
@@ -9,14 +9,32 @@ export const commonChars = (A: string[]): string[] => {
   const flattened = charsArr.reduce((acc, cur) => [...acc, ...cur], []);
 
   const counted = flattened.reduce((all: Object, str: string) => {
-    if (str in all) all[str] += 1;
-    else all[str] = 1;
+    if (str in all) {
+      all[str] += 1;
+    } else {
+      all[str] = 1;
+    }
 
     return all;
   }, {});
 
   Object.entries(counted).forEach(([key, value]) => {
     if (value === A.length) result.push(key);
+  });
+
+  // including duplicates
+  result.forEach((alphabet) => {
+    const same = new Set();
+    A.forEach(item => {
+      const num = Array.from(item).filter(i => i === alphabet).length;
+      same.add(num);
+    });
+
+    if (same.size === 1) {
+      for (let index = 1; index < same.values().next().value; index++) {
+        result.push(alphabet);
+      }
+    }
   });
 
   return result;
