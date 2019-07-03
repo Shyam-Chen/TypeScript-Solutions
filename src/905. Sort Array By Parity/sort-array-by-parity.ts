a8ba7c17a0f6fb9ea0e3f532ece6a8d54a9ccd45
Shyam Chen <shyamchen1994@gmail.com>
2019-07-03 13:59:08 +0800

    16th Commit

--- src/905. Sort Array By Parity/sort-array-by-parity.ts
+++ src/905. Sort Array By Parity/sort-array-by-parity.ts
@@ -1,17 +1,16 @@
 export const sortArrayByParityS1 = (arr: number[]): number[] => {
   const result = [];
 
-  arr.forEach((item) => {
+  arr.forEach(item => {
     if (item % 2 === 0) result.push(item);
   });
 
-  arr.forEach((item) => {
+  arr.forEach(item => {
     if (item % 2 === 1) result.push(item);
   });
 
   return result;
 };
 
-export const sortArrayByParityS2 = (arr: number[]): number[] => (
-  arr.sort((x, y) => x % 2 - y % 2)
-);
+export const sortArrayByParityS2 = (arr: number[]): number[] =>
+  arr.sort((x, y) => (x % 2) - (y % 2));
