ddca302e344df7039ea62eae71ea121ee3538788
Shyam Chen <shyamchen1994@gmail.com>
2019-07-07 18:21:36 +0800

    22nd Commit

--- src/Easy/283. Move Zeroes/move-zeroes.spec.ts
+++ src/Easy/283. Move Zeroes/move-zeroes.spec.ts
@@ -1,7 +1,11 @@
-import { moveZeroes } from './move-zeroes';
+import { moveZeroes, moveZeroes2 } from './move-zeroes';
 
-describe('moveZeroes', () => {
+describe('283. Move Zeroes', () => {
   it('moveZeroes', () => {
     expect(moveZeroes([0, 1, 0, 3, 12])).toEqual([1, 3, 12, 0, 0]);
+  });
+
+  it('moveZeroes2', () => {
+    expect(moveZeroes2([0, 1, 0, 3, 12])).toEqual([1, 3, 12, 0, 0]);
   });
 });
--- src/Easy/283. Move Zeroes/move-zeroes.ts
+++ src/Easy/283. Move Zeroes/move-zeroes.ts
@@ -1,3 +1,23 @@
 export const moveZeroes = (nums: number[]): number[] => (
   [...nums.filter(x => x !== 0), ...nums.filter(x => x === 0)]
 );
+
+export const moveZeroes2 = (nums: number[]): number[] => {
+  let [left, right] = [0, 0];
+
+  while (right < nums.length) {
+    if (nums[right] === 0) {
+      right += 1;
+    } else {
+      const temp = nums[right];
+
+      nums[right] = nums[left];
+      nums[left] = temp;
+
+      left += 1;
+      right += 1;
+    }
+  }
+
+  return nums;
+};
