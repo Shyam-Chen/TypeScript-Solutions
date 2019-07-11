9619a55ede5bcb4c13b7f20f9e3fda8fc47f29f2
Shyam Chen <shyamchen1994@gmail.com>
2019-07-11 17:08:41 +0800

    28th Commit

--- src/Medium/59. Spiral Matrix II/spiral-matrix-2.ts
+++ src/Medium/59. Spiral Matrix II/spiral-matrix-2.ts
@@ -10,7 +10,7 @@ export const generateMatrix = (n: number): number[][] => {
   let [up, down, left, right, step] = [0, 0, 0, 0, 0];
 
   while (true) {
-    // up
+    // up (left -> right)
     for (let i = left; i < n - right; i += 1) {
       step += 1;
       result[up][i] = step;
@@ -19,7 +19,7 @@ export const generateMatrix = (n: number): number[][] => {
     up += 1;
     if (up + down === n) break;
 
-    // right
+    // right (up -> down)
     for (let i = up; i < n - down; i += 1) {
       step += 1;
       result[i][n - 1 - right] = step;
@@ -28,7 +28,7 @@ export const generateMatrix = (n: number): number[][] => {
     right += 1;
     if (left + right === n) break;
 
-    // down
+    // down (right -> left)
     for (let i = n - 1 - right; i >= left; i -= 1) {
       step += 1;
       result[n - 1 - down][i] = step;
@@ -37,7 +37,7 @@ export const generateMatrix = (n: number): number[][] => {
     down += 1;
     if (up + down === n) break;
 
-    // left
+    // left (down -> up)
     for (let i = n - 1 - down; i >= up; i -= 1) {
       step += 1;
       result[i][left] = step;
