afd080099f16d5126aca7f1bdcd1f6867f9d7357
Shyam-Chen <shyamchen1994@gmail.com>
2021-05-14 16:18:38 +0800

    108th Commit

---
+++ src/page16/1752. Check if Array Is Sorted and Rotated/check.ts
@@ -0,0 +1,15 @@
+interface Check {
+  (nums: number[]): boolean;
+}
+
+export const check: Check = (nums) => {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] > nums[(i + 1) % nums.length]) {
+      count += 1;
+    }
+  }
+
+  return count <= 1;
+};
