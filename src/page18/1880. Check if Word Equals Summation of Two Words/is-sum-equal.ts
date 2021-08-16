858395b5b79c573875239bc118d7b68365dbe7ec
Shyam-Chen <shyamchen1994@gmail.com>
2021-08-16 09:44:10 +0800

    110th Commit

---
+++ src/page18/1880. Check if Word Equals Summation of Two Words/is-sum-equal.ts
@@ -0,0 +1,13 @@
+interface IsSumEqual {
+  (firstWord: string, secondWord: string, targetWord: string): boolean;
+}
+
+export const isSumEqual: IsSumEqual = (firstWord, secondWord, targetWord) => {
+  const sum = (str: string): number =>
+    Array.from(str).reduce((acc, cur) => Number(acc) + (cur.charCodeAt(0) - 'a'.charCodeAt(0)), 0);
+
+  const source = sum(firstWord) + sum(secondWord);
+  const target = sum(targetWord);
+
+  return source === target;
+};
